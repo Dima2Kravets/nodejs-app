13c977c88e2b5aeb34e286e22d52ba81f444b57a
Dima_Kravets <dima2.kravets@gmail.com>
2025-10-13 12:07:44 +0300

    feat: initialize Node.js application with Express, CORS, and logging middleware
    - Created package.json with necessary dependencies and scripts
    - Implemented server.js with basic user management routes
    - Added middleware for JSON parsing, CORS, logging, and error handling
    - Set up a root route and a route for retrieving and creating users
    - Included a test route to demonstrate error handling

---
+++ src/server.js
@@ -0,0 +1,84 @@
+// src/server.js
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+let users = [];
+
+// Middleware для парсингу JSON
+app.use(express.json());
+app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+// Логування часу
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
+
+// Кореневий маршрут
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello, World!' });
+});
+
+app.get('/users', (req, res) => {
+  res.status(200).json(users);
+});
+
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  if (!newUser.name) {
+    return res.status(400).json({ message: 'Name is required' });
+  }
+
+  users.push(newUser); // додаємо у пам’ять
+  res.status(201).json({ message: 'User created', user: newUser });
+});
+
+// Маршрут для тестування middleware помилки
+app.get('/test-error', (req, res) => {
+  // Штучна помилка для прикладу
+  throw new Error('Something went wrong');
+});
+
+// Middleware 404 (після всіх маршрутів)
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Middleware для обробки помилок (останнє)
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
